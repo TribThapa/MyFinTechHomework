e91a871aa196b208a4613c104d927aa2ac9d7323
tthapa3 <tthapa3@yahoo.com>
2021-09-18 08:17:44 +1000

    Week109_HW

---
+++ Week19_13092021/Assignment/README.md
@@ -0,0 +1,42 @@
+# MyFinTechHomework
+
+![ProgrammingPic](https://github.com/TribT/MyFinTechHomework/blob/main/images/FinTech.gif)
+
+### My name is Tribikram Thapa (Rana). I have a background in Physiotherapy, and currently work as a Research Fellow at Monash University.
+
+#### I aim to transition into finance with the hope of using blockchain technology in the banking sector. I chose FinTech as a course because it combines my passion in programming and finance.
+
+##### This repository will contain my homework for units taught weekly. The folder names below are hyperlinked to their respective directories. 
+
+### Table of contents
+
+- [code](https://github.com/TribThapa/MyFinTechHomework/tree/main/code/)
+- [data](https://github.com/TribThapa/MyFinTechHomework/tree/main/data/)
+- [images](https://github.com/TribThapa/MyFinTechHomework/tree/main/images/)
+- [pets](https://github.com/TribThapa/MyFinTechHomework/tree/main/pets/)
+- [references](https://github.com/TribThapa/MyFinTechHomework/tree/main/references/)
+- [Week1_10052021](https://github.com/TribThapa/MyFinTechHomework/tree/main/Week1_10052021/)
+- [Week2_17052021](https://github.com/TribThapa/MyFinTechHomework/tree/main/Week2_17052021/)
+- [Week3_24052021](https://github.com/TribThapa/MyFinTechHomework/tree/main/Week3_24052021/)
+- [Week4_31052021](https://github.com/TribThapa/MyFinTechHomework/tree/main/Week4_31052021/)
+- [Week5_07062021](https://github.com/TribThapa/MyFinTechHomework/tree/main/Week5_07062021/)
+- [Week6_14062021](https://github.com/TribThapa/MyFinTechHomework/tree/main/Week6_14062021/)
+- [Week7_21062021](https://github.com/TribThapa/MyFinTechHomework/tree/main/Week7_21062021/)
+- [Week8_28062021](https://github.com/TribThapa/MyFinTechHomework/tree/main/Week8_28062021/)
+- [Week10_12072021](https://github.com/TribThapa/MyFinTechHomework/tree/main/Week10_12072021/)
+- [Week11_19072021](https://github.com/TribThapa/MyFinTechHomework/tree/main/Week11_19072021/)
+- [Week12_26072021](https://github.com/TribThapa/MyFinTechHomework/tree/main/Week12_26072021/)
+- [Week13_02082021](https://github.com/TribThapa/MyFinTechHomework/tree/main/Week13_02082021/)
+- [Week14_09082021](https://github.com/TribThapa/MyFinTechHomework/tree/main/Week14_09082021/)
+- [Week15_16082021](https://github.com/TribThapa/MyFinTechHomework/tree/main/Week15_16082021/)
+- [Week18_06092021](https://github.com/TribThapa/MyFinTechHomework/tree/main/Week18_06092021/)
+- [Week19_13092021](https://github.com/TribThapa/MyFinTechHomework/tree/main/Week19_13092021/)
+- [Week20_20092021](https://github.com/TribThapa/MyFinTechHomework/tree/main/Week20_20092021/)
+- [Week21_27092021](https://github.com/TribThapa/MyFinTechHomework/tree/main/Week21_27092021/)
+
+
+<!--- [![Thapa's GitHub stats](https://github-readme-stats.vercel.app/api?username=TribT&show_icons=true&theme=dark)](https://github.com/TribT/github-readme-stats)--->
+
+![Thapa's GitHub stats](https://github-readme-stats.vercel.app/api?username=TribThapa&theme=dark&show_icons=true&title_color=Blue)
+
+[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=TribThapa&layout=compact&theme=dark&title_color=Blue)](https://github.com/TribThapa/github-readme-stats)
--- Week19_13092021/Assignment/wallet.py
+++ Week19_13092021/Assignment/wallet.py
@@ -6,49 +6,65 @@ from dotenv import load_dotenv
 import os 
 from dotenv import load_dotenv
 from pathlib import Path
+from pprint import pprint
 
 from constants import *
 from web3 import Web3
 from eth_account import Account 
 
 from web3.middleware import geth_poa_middleware
+from web3 import Web3, middleware, Account
+from web3.gas_strategies.time_based import medium_gas_price_strategy
+from web3.middleware import geth_poa_middleware
+
 from bit import PrivateKeyTestnet
 from bit.network import NetworkAPI
 
 load_dotenv()
 
-mnemonic = os.getenv('MNEMONIC','mass dentist model lounge congress lesson caught exotic lava select silent divert')
 
-coins = {BTC, ETH, BTCTEST}
+# connect to Web3
+w3 = Web3(Web3.HTTPProvider('http://localhost:8545'))
 
-def derive_wallets(mnemonic, coin, numderive):
-    command = f"php./hd-wallet-derive/hd-wallet-derive.php -g --mnemonic='{mnemonic}' --coin='{coin}'  --numderive='{numderive}' --cols=path,address,privkey,pubkey --format=json"
+# enable PoA middleware
+w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+w3.eth.setGasPriceStrategy(medium_gas_price_strategy)
 
+# get mnemonic
+mnemonic = os.getenv('mnemonic')
+
+# get coins from constants.py file
+coin = BTC
+
+# print results
+print(coins)
+print()
+print(type(mnemonic))
+
+
+
+# func for wallet
+def derive_wallets(mnemonic, coin, numderive=3):
+    command = f"./derive -g --mnemonic='{mnemonic}' --coin='{coin}'  --numderive='{numderive}' --cols=path,address,privkey,pubkey --format=json"
     p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-    output, err = p.communicate()
+    (output, err) = p.communicate()
     p_status = p.wait()
-
-    keys = json.loads(output)
-    
+    keys = json.loads(output) 
     return print(keys)
 
 
-
-
+    
+# func for coins
 def coins():
     coin_dict = {}
     
-    for i in coins:
-        
-        coin_dict[i] = derive_wallets(mnemonic, i, 3)
-        
+    for i in coins:        
+        coin_dict[i] = derive_wallets(mnemonic, i, 3)        
     return coin_dict
 
 
 
-
-
-
+# convert the `privkey` string in a child key to an account object that `bit` or `web3.py` can use to transact
 def priv_key_to_account(coin, priv_key):
     if coin == ETH:
         private_key = Account.privateKeyToAccount(priv_key)
@@ -58,37 +74,36 @@ def priv_key_to_account(coin, priv_key):
 
 
 
-
-
-
+# create the raw, unsigned transaction that contains all metadata needed to transact
 def create_tx(coin, account, to, amount):
     if coin=='eth':
-        gasEstimate = w3.eth.estimateGas({"from": account.address, "to": to, "value": amount})
-        return { "from": account.address,
+        value = w3.toWei(amount, "ether")
+        gasEstimate = w3.eth.estimateGas({"from": account.address, "to": to, "amount": value})
+        return {"from": account.address,
                 "to": to,
-                "value": amount,
+                "value": value,
                 "gas": gasEstimate,
-                "gasPrice": w3.eth.gasPrice,
-                "nonce": w3.eth.getTransactionCount(account.address)}
+                "gasPrice": w3.eth.generateGasPrice(),
+                "nonce": w3.eth.getTransactionCount(account.address),
+                "chainID": w3.eth.chain_id}
     
     elif coin=='btc-test':        
         return PrivateKeyTestnet.prepare_transaction(account.address, [(to, amount, BTC)])
     
     
-    
-    
-    
+# call `create_tx`, sign the transaction, then send it to the designated network    
 def send_tx(coin, account, to, amount):
+    if coin == ETH:
+        raw_tx = create_tx(coin, account.address, to, amount)
+        signed = account.signTransaction(raw_tx)
+        return w3.eth.sendRawTransaction(signed.rawTransaction)
+    if coin == BTCTEST:
+        raw_tx = create_tx(coin, account, to, amount)
+        signed = account.sign_transaction(raw_tx)
+        return NetworkAPI.broadcast_tx_testnet(signed)    
     
-    tx = create_tx(coin, account, to, amount)
     
-    signed_tx = account.sign_transaction(tx)
-    
-    if coin=='eth':
-        result = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
-        
-        return result.hex()
-    
-    elif coin=='btc-test':
-        
-        return NetworkAPI.broadcast_tx_testnet(signed_tx) 
+coins = {ETH: derive_wallets(mnemonic, coin),
+         BTCTEST: derive_wallets(mnemonic, coin)}
+
+pprint(coins)

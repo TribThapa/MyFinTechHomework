280892c6b3bc4c3d8e84b6e147a74f70e27eed6b
tthapa3 <tthapa3@yahoo.com>
2021-09-23 21:03:43 +1000

    Week20_Day2

--- Week20_20092021/Day2/CustomerSavings_class.sol
+++ Week20_20092021/Day2/CustomerSavings_class.sol
@@ -6,12 +6,20 @@ contract CustomerSavings {
     // an address that is not payable cannot send ETH
     address payable account_one = 0x66bF885018A1948262266084d6f372c68fE35061;
     address payable account_two = 0x65a52a37885AB21770827202Fe80C89DAd343f5c;
+    
+    uint public balanceContract;
   
+  // transfer is part of the 'payable' package
   function withdraw(uint amount, address payable recipient) public {
-    return recipient.transfer(amount); // transfer is part of the 'payable' package
+    recipient.transfer(amount); 
+    balanceContract = address(this).balance;
   }
+  
+  // payable allows you to move ETH around
+  function deposit() public payable {
+      balanceContract = address(this).balance;
+  } 
 
-  function deposit() public payable {} // payable allows you to move ETH around
-
-  function() external payable {} // ensures the smart contract doesn't hold the ETH but rather pays to the addresses from A-to-B
-
+  function() external payable {} // for you to get your money back 
+  
+}
